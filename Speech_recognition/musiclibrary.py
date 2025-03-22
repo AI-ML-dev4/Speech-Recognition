884b4a4f251aa9e48260c4275f7035439d53ab17
AI-ML-dev4 <prateeksinghrajawat7@gmail.com>
2025-03-22 09:17:31 +0530

    add files

---
+++ Speech_recognition/musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "rang" : "https://www.youtube.com/watch?v=OdrOp7JwZiE",
+    "chand" : "https://www.youtube.com/watch?v=7c3-Gei5j4w&pp=ygUNY2hhbmQgYmFsaXlhbg%3D%3D",
+    "tere" : "https://www.youtube.com/watch?v=qYjPxdLIuk0&pp=ygUKdGVyZSBuYWluYQ%3D%3D"
+}
